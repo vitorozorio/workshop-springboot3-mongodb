457d344fda6dfa4d817adb26bd368903298bb6ff
Vitor ozorio <vitormanoelbasilio357@gmail.com>
2024-08-09 13:44:22 -0300

    first commit

---
+++ src/main/java/com/nelioalves/WorkshopMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbApplication.class, args);
+	}
+
+}
