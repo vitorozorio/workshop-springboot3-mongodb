5e7c8f22f6e370dfe8f6f8dc9e880834e05b7782
vitor ozorio <vitormanoelbasilio357@gmail.com>
2024-09-25 20:14:47 -0300

    Referencing user posts

--- src/main/java/com/nelioalves/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongodb/config/Instantiation.java
@@ -38,7 +38,10 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem","Vou viajar para São Paulo, Abraços", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia","Um ótimo dia", new AuthorDTO(maria));
-        
+
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
